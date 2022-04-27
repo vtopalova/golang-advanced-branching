2bbc3bafdea2ea6755ceff28b67d9c9763a43061
I549296 <valentina.topalova@sap.com>
2022-04-27 22:51:24 +0200

    Implemented module 1.

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,39 @@
 package main
 
-import (
-
-)
-
-
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json:"values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct {
+	feedbackTotal    int
+	feedbackPositive int
+	feedbackNeutral  int
+	feedbackNegative int
+}
+
+type car struct {
+	model       string
+	make        string
+	typeVehicle string
+}
+
+type truck struct {
+	model       string
+	make        string
+	typeVehicle string
+}
+
+type bike struct {
+	model string
+	make  string
+}
 
 type rating float32
 
@@ -19,9 +45,14 @@ const (
 	extraNegative rating = -1.2
 )
 
+type vehicle interface {
+}
+
+var vehicleResult map[string]feedbackResult
+var inventory []vehicle
+
 func init() {
 
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,15 +65,15 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
